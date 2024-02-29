b91fc7af00d671fb91bceed41353c7c38c4cc2a9
Radek <146724612+Radzio310@users.noreply.github.com>
2024-02-29 13:07:50 +0100

    Add files via upload

---
+++ C_Zadanie 1.1.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	/*string imie;
+	cout << "Podaj imie: ";
+	cin >> imie;
+	cout << "Twoje imie to " << imie << endl;*/
+	char imie[50];
+	printf("Podaj imie: ");
+	scanf_s("%s", imie, sizeof(imie));
+	printf("Twoje imie to %s", imie);
+}
