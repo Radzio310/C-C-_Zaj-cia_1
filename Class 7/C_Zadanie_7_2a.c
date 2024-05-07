21ad6355b8e5f352ed790c85594942ab6332326b
Radosław Witkowicz <radoslaw.witkowicz@edu.uekat.pl>
2024-05-07 12:18:39 +0200

    Class 7 Zadanie_7_2a.c repaired FINALLY!

--- Class 7/C_Zadanie_7_2a.c
+++ Class 7/C_Zadanie_7_2a.c
@@ -4,18 +4,13 @@
 
 #define MAX_LINE_LENGTH 200
 
-int znajdzSlowo(char *napis, char *slowo) {
-    char *wynik = strstr(napis, slowo);
-    return (wynik != NULL); // Zwraca 1, jeśli słowo zostało znalezione, 0 w przeciwnym razie
-}
-
 int main()
 {
     char linia[MAX_LINE_LENGTH];
 
     FILE *fp;
     fp = fopen("input_2.txt", "r");
-    if (fp == NULL) // czy plik został otwarty poprawnie
+    if (fp == NULL)
     {
         printf("Blad otwarcia pliku.\n");
         return 1; // NIEPOWODZENIE
@@ -25,78 +20,69 @@ int main()
     int suma_part1 = 0;
     int suma_mocy = 0;
 
-    while (fgets(linia, 200, fp) != NULL)
+    while (fgets(linia, MAX_LINE_LENGTH, fp) != NULL)
     {
         int max_red = 0, max_green = 0, max_blue = 0;
-        int num = 0;
-        int linelength = 1;
+        int num;
 
-        // rzeczywista długość linii
-        for(int i = 0; i < MAX_LINE_LENGTH; i++) {
-            if(linia[i] != NULL) {
-                linelength++;
-            }
-        }
+        // Oblicz rzeczywistą długość linii
+        int linelength = strlen(linia);
 
-        for (int i = 8; i < linelength; i++) // Iterujemy przez znaki w linii do napotkania znaku końca linii
+        for (int i = 8; i < linelength;)
         {
-            char znak = linia[i];
-            if (isdigit(znak))
+            if (isdigit(linia[i]))
             {
-                if (i > 0 && isdigit(linia[i + 1]))
+                if (isdigit(linia[i + 1]))
                 {
-                    num = ( (linia[i] - '0') * 10) + (linia[i+1] -'0'); // liczba dwucyfrowa
-                    i++;
+                    num = ((linia[i] - '0') * 10) + (linia[i + 1] - '0');
+                    i += 2; // Przesuń o 2, bo to liczba dwucyfrowa
                 }
                 else
                 {
-                    num = znak - '0';
-                }
-            } else if (isalpha(znak))
-            {
-                for (int j = 0; j < 5; j++)
-                {
-                    char slowo[6]; // Maksymalna długość słowa to 5 liter
-                    for (int k = 0; k < 5 && (i + k) < 100; k++)
-                    {
-                        slowo[k] = linia[i + k]; // Tworzymy słowo z następnych 5 liter w napisie
-                        //slowo[k + 1] = '\0'; // Kończymy słowo znakiem null
-                        if (znajdzSlowo(slowo, "red"))
-                        {
-                            if (num > max_red)
-                            {
-                                max_red = num;
-                                i += k + 1;
-                            }
-                        } else if (znajdzSlowo(slowo, "green"))
-                        {
-                            if (num > max_green)
-                            {
-                                max_green = num;
-                                i += k + 1;
-                            }
-                        } else if (znajdzSlowo(slowo, "blue"))
-                        {
-                            if (num > max_blue)
-                            {
-                                max_blue = num;
-                                i += k + 1;
-                            }
-                        }
-                    }
+                    num = linia[i] - '0';
+                    i++; // Przesuń o 1, bo to liczba jednocyfrowa
                 }
             }
+            else if (strncmp(&linia[i], "red", 3) == 0)
+            {
+                if (num > max_red)
+                {
+                    max_red = num;
+                }
+                i += 3; // Przesuń o 3, bo to słowo kluczowe "red"
+            }
+            else if (strncmp(&linia[i], "green", 5) == 0)
+            {
+                if (num > max_green)
+                {
+                    max_green = num;
+                }
+                i += 5; // Przesuń o 5, bo to słowo kluczowe "green"
+            }
+            else if (strncmp(&linia[i], "blue", 4) == 0)
+            {
+                if (num > max_blue)
+                {
+                    max_blue = num;
+                }
+                i += 4; // Przesuń o 4, bo to słowo kluczowe "blue"
+            }
+            else
+            {
+                i++; // Przesuń o 1, jeśli nie znaleziono żadnego klucza
+            }
         }
-        printf("Game %d: red: %d, green: %d, blue: %d\n", licznik, max_red, max_green, max_blue);
+
         // CZĘŚĆ 1
-        if(max_red <= 12 && max_green <= 13 && max_blue <= 14)
+        if (!(max_red > 12 || max_green > 13 || max_blue > 14))
         {
             suma_part1 += licznik;
-            //printf("Game %d: red: %d, green: %d, blue: %d\n", licznik, max_red, max_green, max_blue);
         }
         // CZĘŚĆ 2
         suma_mocy += (max_red * max_green * max_blue);
         licznik++;
+
+        printf("Game %d: red: %d, green: %d, blue: %d\n", licznik, max_red, max_green, max_blue);
     }
     fclose(fp);
     printf("\n---------------\nSuma identyfikatorow (czesc 1): %d\n", suma_part1);
